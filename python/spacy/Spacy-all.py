471078b0a427912f1cdd286b46181a6dfaa187cc
khashab2 <khashab2@illinois.edu>
2018-02-05 22:50:36 -0600

    adding spacy everything.

---
+++ python/spacy/Spacy-all.py
@@ -0,0 +1,27 @@
+import os
+
+from get_folder import getFolder
+
+root_dir = getFolder()
+files = [item for item in os.listdir(root_dir) if os.path.isfile(os.path.join(root_dir, item))]  # Filter the items and only keep files (strip out directories)
+
+# Install: pip install spacy && python -m spacy download en
+import spacy
+
+# Load English tokenizer, tagger, parser, NER and word vectors
+nlp = spacy.load('en')
+nlp.disable_pipes('ner')
+print(nlp.pipe_names)
+
+
+for ff in files:
+    f = open(root_dir + ff, 'r')
+    paragraph = f.read()
+    doc = nlp(paragraph)
+    sentences = list(doc.sents)
+    [tok for tok in doc]
+    [chunk  for chunk in doc.noun_chunks]
+    [tok.lemma_ for tok in doc]
+    [entity.text for entity in doc.ents]
+    [tok.pos_ for tok in doc]
+    f.close()
