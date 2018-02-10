b2751a39a2c0d5ab1731a074f820ffb8a8a0f5e6
khashab2 <khashab2@illinois.edu>
2018-02-10 13:07:23 -0500

    adding cogcomp evaluation.

--- src/main/java/org/cogcomp/CogComp2ndTry.java
+++ src/main/java/org/cogcomp/CogComp2ndTry.java
@@ -2,17 +2,13 @@ package org.cogcomp;
 
 import edu.illinois.cs.cogcomp.annotation.AnnotatorException;
 import edu.illinois.cs.cogcomp.annotation.AnnotatorService;
+import edu.illinois.cs.cogcomp.core.datastructures.ViewNames;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.TextAnnotation;
-import edu.illinois.cs.cogcomp.nlp.tokenizer.StatefulTokenizer;
-import edu.illinois.cs.cogcomp.nlp.tokenizer.Tokenizer;
 import edu.illinois.cs.cogcomp.pipeline.main.PipelineFactory;
-import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Properties;
 
 // org.cogcomp.CogComp2ndTry
 
@@ -25,39 +21,94 @@ public class CogComp2ndTry {
 
     public static void main(String[] args) throws IOException, AnnotatorException {
         System.out.println("Starting to execute thins ... ");
-        if(args[0].contains("tokenizer1")) {
-            tokenizer1();
+        ner();
+
+        if(args[0].contains("tokenizer")) {
+            tokenizer();
         }
-        else if(args[0].contains("tokenizer2")) {
-            tokenizer2();
+        else if(args[0].contains("pos")) {
+            pos();
+        }
+        else if(args[0].contains("lemma")) {
+            lemma();
+        }
+        else if(args[0].contains("chunker")) {
+            chunker();
+        }
+        else if(args[0].contains("ner")) {
+            ner();
         }
     }
 
-    public static void tokenizer1() throws IOException, AnnotatorException {
+    public static void tokenizer() throws IOException, AnnotatorException {
         File folder = new File(f);
         File[] listOfFiles = folder.listFiles();
         AnnotatorService pipeline = PipelineFactory.buildPipeline();
         for (int i = 0; i < listOfFiles.length; i++) {
             if (listOfFiles[i].isFile() && listOfFiles[i].getName().contains(".txt")) {
                 String content = new String(Files.readAllBytes(Paths.get(listOfFiles[i].getPath())));
-                pipeline.createAnnotatedTextAnnotation( "", "", content);
+                TextAnnotation ta = pipeline.createAnnotatedTextAnnotation( "", "", content);
             } else if (listOfFiles[i].isDirectory()) {
                 System.out.println("Directory " + listOfFiles[i].getName());
             }
         }
     }
 
-    public static void tokenizer2() throws IOException, AnnotatorException {
+    public static void pos() throws IOException, AnnotatorException {
         File folder = new File(f);
         File[] listOfFiles = folder.listFiles();
-        Tokenizer t = new StatefulTokenizer(true);
+        AnnotatorService pipeline = PipelineFactory.buildPipeline(ViewNames.POS);
         for (int i = 0; i < listOfFiles.length; i++) {
             if (listOfFiles[i].isFile() && listOfFiles[i].getName().contains(".txt")) {
                 String content = new String(Files.readAllBytes(Paths.get(listOfFiles[i].getPath())));
-                t.tokenizeSentence(content);
+                TextAnnotation ta = pipeline.createAnnotatedTextAnnotation("", "", content);
             } else if (listOfFiles[i].isDirectory()) {
                 System.out.println("Directory " + listOfFiles[i].getName());
             }
         }
     }
+
+    public static void lemma() throws IOException, AnnotatorException {
+        File folder = new File(f);
+        File[] listOfFiles = folder.listFiles();
+        AnnotatorService pipeline = PipelineFactory.buildPipeline(ViewNames.POS, ViewNames.LEMMA);
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile() && listOfFiles[i].getName().contains(".txt")) {
+                String content = new String(Files.readAllBytes(Paths.get(listOfFiles[i].getPath())));
+                TextAnnotation ta = pipeline.createAnnotatedTextAnnotation("", "", content);
+            } else if (listOfFiles[i].isDirectory()) {
+                System.out.println("Directory " + listOfFiles[i].getName());
+            }
+        }
+    }
+
+    public static void chunker() throws IOException, AnnotatorException {
+        File folder = new File(f);
+        File[] listOfFiles = folder.listFiles();
+        AnnotatorService pipeline = PipelineFactory.buildPipeline(ViewNames.POS, ViewNames.LEMMA,  ViewNames.SHALLOW_PARSE);
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile() && listOfFiles[i].getName().contains(".txt")) {
+                String content = new String(Files.readAllBytes(Paths.get(listOfFiles[i].getPath())));
+                TextAnnotation ta = pipeline.createAnnotatedTextAnnotation("", "", content);
+            } else if (listOfFiles[i].isDirectory()) {
+                System.out.println("Directory " + listOfFiles[i].getName());
+            }
+        }
+    }
+
+    public static void ner() throws IOException, AnnotatorException {
+        File folder = new File(f);
+        File[] listOfFiles = folder.listFiles();
+        AnnotatorService pipeline = PipelineFactory.buildPipeline(ViewNames.POS, ViewNames.LEMMA, ViewNames.SHALLOW_PARSE, ViewNames.NER_CONLL);
+        System.out.println("Starting to evaluate . . . ");
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile() && listOfFiles[i].getName().contains(".txt")) {
+                String content = new String(Files.readAllBytes(Paths.get(listOfFiles[i].getPath())));
+                TextAnnotation ta = pipeline.createAnnotatedTextAnnotation("", "", content);
+            } else if (listOfFiles[i].isDirectory()) {
+                System.out.println("Directory " + listOfFiles[i].getName());
+            }
+        }
+    }
+
 }
