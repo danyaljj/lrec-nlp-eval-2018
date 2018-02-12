1d321fbd8f61d04574f1d9c56f689ddecc1a2bc1
Daniel Khashabi <khashab2@illinois.edu>
2018-02-12 10:26:06 -0500

    Update get_folder.py

--- python/spacy/get_folder.py
+++ python/spacy/get_folder.py
@@ -2,4 +2,4 @@
 def getFolder():
     # return "/Users/daniel/Desktop/corpusFilesPlain/"
     return "/shared/shelley/khashab2/corpusFilesPlain/"
-    return "/scratch/lrec-eval/corpusFilesPlain/"
+    # return "/scratch/lrec-eval/corpusFilesPlain/"
